ce4f08f3dae1755c7591dbc6e5db6691ef923e68
Juan Pablo Cabrera <jpcq2002@yahoo.com.mx>
2024-08-16 09:36:19 -0600

    updating params in order to keep it consistent with the the terms we are using in class

--- Model_Creation/linear_regression.py
+++ Model_Creation/linear_regression.py
@@ -1,25 +1,26 @@
 import numpy as np
 
-def hypothesis(params, sample, intercept):
+def hypothesis(params, sample, bias):
     """
     Calcula la hipótesis de una ecuación lineal. Sumo el bias al final de la ecuación.
     Me parece mejor de esta forma en vez de hacer el arreglo con 1 al principio.
     """
-    return np.dot(params, sample) + intercept
+    return np.dot(params, sample) + bias
 
-def cost_function(params, intercept, samples, y):
+
+def cost_function(params, bias, samples, y):
     """
     Calculo la función del MSE, es la que hemos visto en clase, nada fancy. Me basé en este código de stack overflow:
     https://stackoverflow.com/questions/16774849/mean-squared-error-in-numpy, pero al platicarlo con benji, me dijo que para que 
     no fallara, tenía que tener mi y_prima ya calulada.
     """
-    y_prima = np.dot(samples, params) + intercept
+    y_prima = np.dot(samples, params) + bias
     mse = np.mean(np.square(y_prima - y)) / 2
     
     return mse
 
 
-def gradient_descent(params, intercept, samples, y, alpha, iterations):
+def gradient_descent(params, bias, samples, y, alpha, iterations):
     """
     Implemento el algoritmo de gradiente descendiente, me base en el código que Beji nos dio de regresion lineal.
     Solo que en vez de hacerlo con listas, lo hago con arrays de numpy directamente.
@@ -27,24 +28,24 @@ def gradient_descent(params, intercept, samples, y, alpha, iterations):
     m = len(y)
 
     for _ in range(iterations):
-        #Le sumo la intercepión a la hipótesis, esto lo platiqué con Benji para no tener que tener un arreglo con 1 al principio.
-        predictions = np.dot(samples, params) + intercept        
+        #Le sumo el bias a la hipótesis, esto lo platiqué con Benji para no tener que tener un arreglo con 1 al principio.
+        predictions = np.dot(params, samples) + bias
         errors = predictions - y
         
         # Calcular los gradientes
         gradient_params = (1/m) * np.dot(samples.T, errors)
-        gradient_intercept = (1/m) * np.sum(errors)
+        gradient_bias = (1/m) * np.sum(errors)
         
-        # Actualizar los parámetros y el intercepto
+        # Actualizar los parámetros y el biaso
         params -= alpha * gradient_params
-        intercept -= alpha * gradient_intercept
-        print(intercept)
+        bias -= alpha * gradient_bias
+        print(bias)
 
-    return params, intercept
+    return params, bias
 
 
 params = np.array([0.0, 0.0]) 
-intercept = 0.0 
+bias = 0.0 
 
 
 samples = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]])
@@ -56,12 +57,12 @@ alpha = 0.01
 iterations = 1000 
 
 
-params, intercept = gradient_descent(params, intercept, samples, y, alpha, iterations)
+params, bias = gradient_descent(params, bias, samples, y, alpha, iterations)
 
 
 print("Parámetros finales:", params)
-print("Intercepto final:", intercept)
+print("Bias final:", bias)
 
 
-final_cost = cost_function(params, intercept, samples, y)
+final_cost = cost_function(params, bias, samples, y)
 print("Costo final:", final_cost)
